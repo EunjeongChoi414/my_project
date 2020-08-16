95cd0e5224acdefd1bba8ff56aa00f8cb537f021
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-08-16 13:13:52 +0900

    우리당장만나 메인페이지

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-from urllib.request import urlretrieve
-from urllib.parse import quote_plus
-from bs4 import BeautifulSoup
-from selenium import webdriver
+from flask import Flask, render_template
 
-search = input('어떤 사진을 원하느냐: ')
-url = f'https://www.google.com/search?q={quote_plus(search)}'
+app = Flask(__name__)
 
-driver = webdriver.Chrome()
-driver.get(url)
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-
-img = soup.select("rg_i Q4LuWd")
-n = 1
-imgUrl = []
-
-for i in img:
-    try:
-        imgUrl.append(i.atttrs["src"])
-    except KeyError:
-        imgUrl.append(i.atttrs['data-src'])
-
-for i in imgUrl:
-    urlretrieve(i, "크롤링사진/" + search + str(n) + ".jpg")
-    n += 1
-
-driver.close()
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
