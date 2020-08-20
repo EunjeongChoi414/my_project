f2471d5631cf1c39489adadbf6a1193c6183eb51
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-08-20 19:35:55 +0900

    페이지 전환

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
@@ -8,6 +8,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/when')
+def when():
+    return render_template('when.html')
+
+
+@app.route('/when2')
+def when2():
+    return render_template('when2.html')
+
+@app.route('/when3')
+def when3():
+    return render_template('when3.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
