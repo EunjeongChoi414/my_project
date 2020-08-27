fed126814627b9b3f090d1c83c3316fdbddbb8d3
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-08-27 14:11:08 +0900

    선택한 날짜 리스트에 저장

--- app.py
+++ app.py
@@ -1,22 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbMeetUp
 
 app = Flask(__name__)
 
 
 # HTML 화면 보여주기
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/', methods=['POST'])
+def write_user_info():
+    leader_name_receive = request.form['leader_name_give']
+    member_name_receive = request.form['member_name_give']
 
+    user = {
+        'id': leader_name_receive,
+        'member': member_name_receive
+    }
+    db.users.insert_one(user)
+    return jsonify({'result': 'success', 'msg': '저장완료'})
 
 @app.route('/when')
 def when():
     return render_template('when.html')
-
+# // 클릭한 날짜 저장하기
 
 @app.route('/when2')
 def when2():
     return render_template('when2.html')
+# //클릭한 날짜 보여주기
+# //선택한 시간 저장하기
 
 @app.route('/when3')
 def when3():
