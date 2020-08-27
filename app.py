31773b63a552dd48b6046cf696ddc192ae9a7db1
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-08-27 22:36:34 +0900

    날짜 db에 저장

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
+import urllib
 
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbMeetUp
@@ -8,22 +9,36 @@ app = Flask(__name__)
 
 
 # HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/', methods=['POST'])
 def write_user_info():
     leader_name_receive = request.form['leader_name_give']
     member_name_receive = request.form['member_name_give']
-
     user = {
         'id': leader_name_receive,
         'member': member_name_receive
     }
     db.users.insert_one(user)
-    return jsonify({'result': 'success', 'msg': '저장완료'})
+    return jsonify({'result': 'success'})
+# // 클릭한 날짜 저장하기
 
 @app.route('/when')
 def when():
     return render_template('when.html')
+
 # // 클릭한 날짜 저장하기
+@app.route('/when', methods=['POST'])
+def save_dates():
+    dates_receive = request.form.getlist('dates_give[]')
+    id_receive = request.form['id_give']
+
+    print(id_receive)
+    print(urllib.parse.unquote(id_receive))
+    db.users.update_one({'id': urllib.parse.unquote(id_receive)}, {'$set': {'ava_dates': dates_receive}})
+    return jsonify({'result': 'success', 'msg': '날짜 저장완료'})
 
 @app.route('/when2')
 def when2():
