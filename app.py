1504e5b5c2e8239b14d3e3d4ca32f53cbb3ae3f7
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-08-31 17:39:28 +0900

    get dates?

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 
 
 # HTML 화면 보여주기
-@app.route('/')
+@app.route('/user_info')
 def home():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/user_info', methods=['POST'])
 def write_user_info():
     leader_name_receive = request.form['leader_name_give']
     member_name_receive = request.form['member_name_give']
@@ -25,30 +25,41 @@ def write_user_info():
     return jsonify({'result': 'success'})
 # // 클릭한 날짜 저장하기
 
-@app.route('/when')
-def when():
-    return render_template('when.html')
+@app.route('/date')
+def date_page():
+    return render_template('date.html')
 
 # // 클릭한 날짜 저장하기
-@app.route('/when', methods=['POST'])
+@app.route('/date', methods=['POST'])
 def save_dates():
     dates_receive = request.form.getlist('dates_give[]')
     id_receive = request.form['id_give']
-
-    print(id_receive)
-    print(urllib.parse.unquote(id_receive))
     db.users.update_one({'id': urllib.parse.unquote(id_receive)}, {'$set': {'ava_dates': dates_receive}})
-    return jsonify({'result': 'success', 'msg': '날짜 저장완료'})
+    return jsonify({'result': 'success'})
 
-@app.route('/when2')
+@app.route('/time')
 def when2():
-    return render_template('when2.html')
-# //클릭한 날짜 보여주기
-# //선택한 시간 저장하기
+    return render_template('time.html')
 
+@app.route('/time', methods=['GET'])
+def get_dates():
+    id_receive = request.args.get('id_give')
+    print(id_receive)
+    # dates = db.users.find_one({'id': id_receive}, {'_id': False})
+    # print(dates)
+    return jsonify({'result': 'success', 'msg': "날짜 가져옴"})
+
+@app.route('/time', methods=['GET'])
+def test_get():
+    title_receive = request.args.get('title_give')
+    print(title_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+# //선택한 시간 저장하기
 @app.route('/when3')
 def when3():
     return render_template('when3.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
